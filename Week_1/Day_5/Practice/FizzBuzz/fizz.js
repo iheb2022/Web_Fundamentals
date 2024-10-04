b038c55a531dbd407f04cc7dc4803c6dfdf1db44
iheb cherni <eheebhob@gmail.com>
2024-10-04 18:26:21 +0100

    FizzBuzz

---
+++ Week_1/Day_5/Practice/FizzBuzz/fizz.js
@@ -0,0 +1,11 @@
+for (var i=1;i<101;i++){
+      var number= i; 
+    if (number % 3 == 0 && number %5==0)
+      console.log(FrizzBuzz)
+    if(number % 3 == 0)
+      console.log("Fizz")
+    if(number % 5 == 0)
+      console.log("Buzz")
+    else (console.log(number))
+
+  
