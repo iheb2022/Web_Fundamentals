ccfc1cdc41d2997cb4b4e98ce1453a2fd1828ae5
iheb cherni <eheebhob@gmail.com>
2024-10-05 11:34:01 +0100

    LoopChallenges

---
+++ Week_1/Day_5/Practice/LoopChallenges/Loop.js
@@ -0,0 +1,33 @@
+for (var i = 1; i <= 20; i++) {
+  if (i % 2 !== 0) {
+      console.log(i);
+  }
+}
+
+// 
+
+
+for (var i = 100; i >= 0; i--) {
+  if (i % 3 === 0) {
+      console.log(i);
+  }
+}
+
+
+// 
+
+
+var value = 4;
+while (value >= -3.5) {
+    console.log(value);
+    value = value -1,5
+}
+
+// 
+
+
+var product = 1;
+for (let i = 1; i <= 12; i++) {
+    product =product*i;
+}
+console.log(product); 
