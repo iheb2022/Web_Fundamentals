fe0a7fb0265f5e9d5efeee39a690e5af8aaa1f35
iheb cherni <eheebhob@gmail.com>
2024-10-08 20:49:06 +0100

    dojoweather

--- Week_2/Day_2/Practice/Java script Tacos/sccript.js
+++ Week_2/Day_2/Practice/Java script Tacos/sccript.js
@@ -1,8 +1,22 @@
-function  pizzaOven(crustType, sauceType, cheeses, toppings) {
+function pizzaOven(crusType, sauceType, cheeses, toppings) {
   var pizza = {};
-  pizza.crustType = crustType;
-  pizza.sauceType =  sauceType;
+  pizza.crusType = crusType;
+  pizza.sauceType = sauceType;
   pizza.cheeses = cheeses;
   pizza.toppings = toppings;
   return pizza;
 }
+
+var p1= pizzaOven("deep dish","traditional",["mozzarella"],["pepperoni","sausage"]);
+console.log(p1);
+
+
+var p2= pizzaOven("hand tossed","marinara",["mozzarella","feta"],["mushrooms","olives","onions"]);
+console.log(p2);
+
+var p3= pizzaOven("neapolitan","Margherita",["San Marzano tomatoes","mozzarella"],["olives","sausage"]);
+console.log(p3)
+
+
+var p4= pizzaOven("Stuffed","traditional",["feta","mozzarella"],["Onions","Pepperoni"]);
+console.log(p4);
--- Week_2/Day_2/Practice/dojoweather/script.js
+++ Week_2/Day_2/Practice/dojoweather/script.js
@@ -1,8 +1,15 @@
-function here(alert){
-  console.log(alert("Loading weather report..."))
+function here(){
+  alert("Loading weather report...")
 }
 
 function ab(){
   var del = document.querySelector(".cookies");
   del.remove();
+}
+var arr=["red","blue","red1","blue1","red2","blue2","red3","blue3"]
+
+function changingtemp(){
+    for(var i=0;i<arr.length;i++){
+        the(arr[i])
+    }
 }
