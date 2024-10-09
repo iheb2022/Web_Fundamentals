a474b4b42da765691cc7d33c11e36c7ddf424cfd
iheb cherni <eheebhob@gmail.com>
2024-10-09 10:20:49 +0100

    profile page

--- Week_2/Day_2/Practice/profile-page/script.js
+++ Week_2/Day_2/Practice/profile-page/script.js
@@ -7,4 +7,17 @@ function hide(element){
   z.remove()
 }
 
+function accept(id) {
+  var element = document.querySelector(id);
+  element.remove();
+  requestSpan.innerText--;
+  connectionSpan.innerText++;
+}
+
+function ignore(id) {
+  var element = document.querySelector(id);
+  element.remove();
+  requestSpan.innerText--;
+}
+
 
